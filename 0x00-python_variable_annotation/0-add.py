bff2ca63e317947ce28a8a2319573a2eef124e0d
BoakyePrincella <princellaboakye2003@gmail.com>
2024-10-10 22:04:24 +0000

    Python_variable_annotation

---
+++ 0x00-python_variable_annotation/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This is a type annoted function"""
+
+
+def add(a: float, b: float) -> float:
+    """Add function that is type annoted
+
+    Args:
+        a (float): first operand
+        b (float): second operand
+
+    Returns:
+        float: The sum of both the operands
+    """
+    return a + b
